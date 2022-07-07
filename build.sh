7f18cb1f364f2757298d7ec30c17177eae8a70c1
mxsgx <mxsgx.id@gmail.com>
2022-07-08 03:20:31 +0700

    Using `msbuild` command
    `xbuild` will deprecated in future

--- build.sh
+++ build.sh
@@ -36,7 +36,6 @@ then
 fi    
 
 # build solution
-xbuild /t:Clean /p:Configuration=ReleaseLinux NoteBookFanControl.sln
-xbuild /t:Build /p:Configuration=ReleaseLinux NoteBookFanControl.sln
+msbuild /t:Clean,Build /p:Configuration=ReleaseLinux NoteBookFanControl.sln
 
 popd 
